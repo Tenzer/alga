09a6dd910c59e795eef91962d8127155004962ac
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2024-06-27 21:35:55 +0100

    Format `flake.nix` with `nixfmt`
    https://github.com/NixOS/nixfmt
    
    It's not configured in CI since it's probably not something many people are
    going to contribute towards, so it's not worth the effort for now.

--- flake.nix
+++ flake.nix
@@ -6,13 +6,18 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, pyproject-nix, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      nixpkgs,
+      pyproject-nix,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         inherit (nixpkgs) lib;
-        project = pyproject-nix.lib.project.loadPoetryPyproject {
-          projectRoot = ./.;
-        };
+        project = pyproject-nix.lib.project.loadPoetryPyproject { projectRoot = ./.; };
 
         overlay = _: prev: {
           python3 = prev.python3.override {
@@ -21,9 +26,7 @@
                 version = "0.13.0";
                 pname = "cfgs";
                 format = "pyproject";
-                nativeBuildInputs = with p.pythonPackages; [
-                  poetry-core
-                ];
+                nativeBuildInputs = with p.pythonPackages; [ poetry-core ];
                 src = p.fetchPypi {
                   inherit version pname;
                   hash = "sha256-zvR+Z/BRJ4Pug+JMwvOeWyO11MoMMrvXIb1k9IY2Zn4=";
@@ -33,7 +36,10 @@
           };
         };
 
-        pkgs = import nixpkgs { inherit system; overlays = [ overlay ]; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+        };
         python = pkgs.python3;
       in
       {
@@ -41,14 +47,14 @@
           let
             arg = project.renderers.withPackages { inherit python; };
             pythonEnv = python.withPackages arg;
-          in pkgs.mkShell {
-            packages = [ pythonEnv ];
-          };
+          in
+          pkgs.mkShell { packages = [ pythonEnv ]; };
 
         packages.default =
           let
             attrs = project.renderers.buildPythonPackage { inherit python; };
-          in python.pkgs.buildPythonPackage attrs;
-        }
+          in
+          python.pkgs.buildPythonPackage attrs;
+      }
     );
 }
