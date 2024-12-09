e406b0e5525476d7294a770c2f1dafbce145ff16
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2024-12-09 19:41:13 +0000

    Add `alga sound-output pick` command
    It makes it much easier to change the sound output device.
    
    I haven't been able to find an API to list the possible sound output devices,
    so I've instead added a hardcoded list of devices which seems useful.

--- src/alga/cli_sound_output.py
+++ src/alga/cli_sound_output.py
@@ -1,9 +1,11 @@
 from typing import Annotated
 
+from pzp import pzp
 from rich import print
 from typer import Argument, Typer
 
 from alga import client
+from alga.types import SoundOutputDevice
 
 
 app = Typer(no_args_is_help=True, help="Audio output device")
@@ -15,6 +17,26 @@ def get() -> None:
 
     response = client.request("ssap://audio/getSoundOutput")
     print(f"The current sound output is [bold]{response['soundOutput']}[/bold]")
+
+
+@app.command()
+def pick() -> None:
+    """Show picker for selecting a sound output device."""
+
+    sound_output_device = pzp(
+        candidates=[
+            SoundOutputDevice("tv_speaker", "TV Speaker"),
+            SoundOutputDevice("external_optical", "Optical Out Device"),
+            SoundOutputDevice("tv_external_speaker", "Optical Out Device + TV Speaker"),
+            SoundOutputDevice("external_arc", "HDMI (ARC) Device"),
+        ],
+        fullscreen=False,
+        layout="reverse",
+    )
+    if sound_output_device:
+        client.request(
+            "ssap://audio/changeSoundOutput", {"output": sound_output_device.id_}
+        )
 
 
 @app.command()
--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 from faker import Faker
 
-from alga.types import Channel, InputDevice
+from alga.types import Channel, InputDevice, SoundOutputDevice
 
 
 def test_channel(faker: Faker) -> None:
@@ -19,3 +19,10 @@ def test_input_device(faker: Faker) -> None:
     input_device = InputDevice({"id": faker.pystr(), "label": faker.pystr()})
 
     assert str(input_device) == f"{input_device.name} ({input_device.id_})"
+
+
+def test_sound_output_device(faker: Faker) -> None:
+    name = faker.pystr()
+    sound_output_device = SoundOutputDevice(faker.pystr(), name)
+
+    assert str(sound_output_device) == name
