86438632ace9d7483da0b4002276314f4d4891b4
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2024-12-23 21:55:52 +0000

    Use poetry2nix to configure Nix Flake
    It means no special configuration is needed for dependencies that are not
    packaged in nixpkgs, and installation becomes faster because wheels are used
    which require less dependencies to be installed.

--- flake.nix
+++ flake.nix
@@ -1,93 +1,45 @@
 {
-  inputs = {
-    pyproject-nix.url = "github:nix-community/pyproject.nix";
-    pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  description = "CLI for remote controlling LG webOS TVs";
 
+  inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
   outputs =
     {
+      self,
       nixpkgs,
-      pyproject-nix,
       flake-utils,
-      ...
+      poetry2nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        inherit (nixpkgs) lib;
-        project = pyproject-nix.lib.project.loadPoetryPyproject { projectRoot = ./.; };
-
-        overlay = _: prev: {
-          python3 = prev.python3.override {
-            packageOverrides = _: p: {
-              cfgs = p.buildPythonPackage rec {
-                version = "0.13.0";
-                pname = "cfgs";
-                format = "pyproject";
-                nativeBuildInputs = with p.pythonPackages; [ poetry-core ];
-                src = p.fetchPypi {
-                  inherit version pname;
-                  hash = "sha256-zvR+Z/BRJ4Pug+JMwvOeWyO11MoMMrvXIb1k9IY2Zn4=";
-                };
-              };
-              pzp = p.buildPythonPackage rec {
-                version = "0.0.25";
-                pname = "pzp";
-                format = "pyproject";
-                nativeBuildInputs = with p.pythonPackages; [
-                  poetry-core
-                  setuptools
-                ];
-                src = p.fetchPypi {
-                  inherit version pname;
-                  hash = "sha256-29OhVEM9By+SQ6Cb+SF/f3157FBDttCxDHHV8kpcx2Y=";
-                };
-              };
-              typer = p.buildPythonPackage rec {
-                version = "0.15.1";
-                pname = "typer";
-                format = "pyproject";
-                nativeBuildInputs = with p.pythonPackages; [
-                  poetry-core
-                  pdm-backend
-                ];
-                propagatedBuildInputs = with p.pythonPackages; [
-                  click
-                  shellingham
-                  rich
-                  typing-extensions
-                ];
-                src = p.fetchPypi {
-                  inherit version pname;
-                  hash = "sha256-oFiMCn+mihl4oGmBhld3j4ar5v9epqv0cvlAoIv+Two=";
-                };
-              };
-
-            };
+        alga =
+          { poetry2nix, lib }:
+          poetry2nix.mkPoetryApplication {
+            projectDir = self;
+            preferWheels = true;
           };
-        };
-
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ overlay ];
+          overlays = [
+            poetry2nix.overlays.default
+            (final: _: {
+              alga = final.callPackage alga { };
+            })
+          ];
         };
-        python = pkgs.python3;
       in
       {
-        devShells.default =
-          let
-            arg = project.renderers.withPackages { inherit python; };
-            pythonEnv = python.withPackages arg;
-          in
-          pkgs.mkShell { packages = [ pythonEnv ]; };
-
-        packages.default =
-          let
-            attrs = project.renderers.buildPythonPackage { inherit python; };
-          in
-          python.pkgs.buildPythonPackage attrs;
+        packages.default = pkgs.alga;
+        legacyPackages = pkgs;
       }
     );
 }
